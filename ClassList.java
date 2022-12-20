412879c75693e64e68b3dac6af3d39eea10acb9c
khan2876 <akhan@bpsk12.org>
2022-12-20 09:43:25 -0500

    Starter code

---
+++ ClassList.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ClassList {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        String input = "";
+         
+        // TODO: initialize classList
+        ArrayList<String> list = new ArrayList();
+
+        System.out.println("Q to Quit, A to Add a name, R to remove, I to insert");
+
+        while( !input.equals("Q") ) {
+            System.out.println("Enter your selection: ");
+            input = keyboard.next();
+
+            if(input.equals("A")) {
+                // TODO: Add a name to the end of the list.
+                System.out.print("Enter a name to add: ");
+                String name = keyboard.next();
+                list.add(name);
+            } else if(input.equals("R")) {
+                // TODO: Remove an item from the list.  
+                // Ask for the index of the item to be removed.  Use nextInt()
+
+            } else if(input.equals("I")) {
+                // TODO: Insert an item into the list at a specific location.
+                // Ask for the location to add to, and the name to be added.
+            } else  {
+                System.out.println("Invalid Selection.");
+            }
+            printList(/* TODO: insert appropriate arguments here (the list) */);
+        }
+    }
+
+    public static void printList(/* TODO: insert appropriate arguments here */) {
+        // TODO: print the list along with the index numbers
+
+    }
+}
